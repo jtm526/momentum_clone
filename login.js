8a39a74ac3979ad857743e0a80c7d0ec51bb97e5
jtm526 <jitae.moon@outlook.com>
2022-05-27 23:36:29 +0900

    Create login.js

---
+++ login.js
@@ -0,0 +1,20 @@
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginButtonClick(event) {
+    event.preventDefault();
+
+    
+
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername == null) {
+
+} else {
+    
+}
