796726d93ae2d7db490f490f55b5ce20cb4e25fb
jtm526 <jitae.moon@outlook.com>
2022-06-04 19:21:38 +0900

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,6 +1,13 @@
+const API_KEY = "b8dd14b7236271d6a7e9879d9bbe4b81";
+
 function getWeather(position) {
-    const latitude = position.latitude;
-    const longitude = position.longitude;
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
+    
+}
+function onError() {
+    alert("Can't find your location.");
 }
 
-navigator.geolocation.getCurrentPosition(getWeather);
+navigator.geolocation.getCurrentPosition(getWeather, onError);
