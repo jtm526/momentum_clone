9381644e459913902833d1d9df93033510418a85
jtm526 <jitae.moon@outlook.com>
2022-06-07 23:45:21 +0900

    Update todo.js

---
+++ todo.js
@@ -0,0 +1,5 @@
+const toDoForm = document.querySelector("#todo-form");
+const toDoInput = document.querySelector("#todo-form input");
+const toDoList = document.querySelector(".todo-lists");
+
+let toDos = [];
